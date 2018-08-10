c655262ca1ef306c2ecc73b6254bde7053652b5f
Bruno Martins <bgcngm@gmail.com>
2018-08-10 14:26:16 +0100

    kenzo: Consider mk_kenzo product via the AOSP way
    Change-Id: I5090b03275ecdbd1e27a992f0129a75d5799a8e6

--- mk_kenzo.mk
+++ mk_kenzo.mk
@@ -1,11 +1,12 @@
 #
-# Copyright (C) 2015 The CyanogenMod Project
+# Copyright 2016 The CyanogenMod Project
+# Copyright 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -21,9 +22,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from kipper device
 $(call inherit-product, device/xiaomi/kenzo/device.mk)
 
-# Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := kenzo
-PRODUCT_NAME := full_kenzo
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+
+# Set those variables here to overwrite the inherited values.
+BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := kenzo
+PRODUCT_DEVICE := kenzo
+PRODUCT_NAME := mk_kenzo
 PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := Redmi Note 3
+TARGET_VENDOR := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Build fingerprint
+BUILD_FINGERPRINT=Xiaomi/kenzo/kenzo:6.0.1/MMB29M/V8.2.1.0.MHOCNDL:user/release-keys
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="kenzo-user 6.0.1 MMB29M V8.2.1.0.MHOCNDL release-keys"
